ea710055642f81487bed8aaa8c9a6bdb351353e6
jstillitano <jstillitano@bmtdt.com.au>
2022-03-15 17:25:51 +1100

    Init commit.

---
+++ IAI-Assignment1/Cell.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IAI_Assignment1
+{
+    internal class Cell
+    {
+        // A Cell will have
+            // Coordinates
+            // Wall or Space.
+    }
+}
