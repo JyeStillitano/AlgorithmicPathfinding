c8a776873b005c0f2beaba664c1e3cf18b46fc89
Jye Stillitano <jstillitano@bmtdt.com.au>
2022-03-19 16:44:20 +1100

    Environment.cs setup.

--- IAI-Assignment1/Environment.cs
+++ IAI-Assignment1/Environment.cs
@@ -6,10 +6,45 @@ using System.Threading.Tasks;
 
 namespace IAI_Assignment1
 {
-    internal class Environment
+    public class Environment
     {
-        // Environment will contain
-            // Size, N * M
-            // List of cells as a 2D array
+        //  Example
+
+        //    01234567890  X
+        //    ------------
+        //  0|  XX   GX X|
+        //  1|R XX    X  |
+        //  2|           |
+        //  3|  X      XG|
+        //  4|  XXXX  XX |
+        //  Y ------------
+
+
+        // Size, N * M
+        int N;
+        int M;
+
+        // List of cells as a 2D array
+        Cell[,] cells;
+
+        // Start Coordinate
+        int StartX;
+        int StartY;
+
+        // One or more Goal Coordinates
+        List<Cell> goals;
+
+        public Environment(string filepath)
+        {
+            // Setup stream reader.
+
+            // Environment Size [N,M] | [Y,X]
+
+            // Agent State Coordinate (x,y)
+
+            // Goal State Coordinates (x,y) | (x,y)
+
+            // Wall Coordinates (x,y,width,depth)
+        }
     }
 }
---
+++ IAI-Assignment1/State.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IAI_Assignment1
+{
+    internal class State
+    {
+    }
+}
